f517610ade1f75283be1517121f0ed341c4d92c1
Furkan Sarihan <sarihan3726@gmail.com>
2023-06-12 14:11:25 +0300

    build: optional clean build

--- build.sh
+++ build.sh
@@ -3,12 +3,35 @@
 set -e
 set -x
 
-rm -rf build
-mkdir build
-pushd build
+BUILD_DIR="build"
+CLEAN_BUILD=false
+
+# Parse command line parameters
+for arg in "$@"; do
+  case $arg in
+    --clean)
+      CLEAN_BUILD=true
+      ;;
+  esac
+done
+
+# Clean build directory if requested
+if [ "$CLEAN_BUILD" = true ]; then
+  rm -rf $BUILD_DIR
+  mkdir $BUILD_DIR
+fi
+
+# Create the build directory if it does not exist
+if [ ! -d "$BUILD_DIR" ]; then
+  mkdir $BUILD_DIR
+fi
+
+pushd $BUILD_DIR
 
 conan install .. --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
 bin/enigine
+
+popd
